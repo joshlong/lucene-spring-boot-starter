5dfe3eef69cc7af319ec42f2c7005ae1830f098c
Josh Long <josh@joshlong.com>
2020-10-06 02:08:46 -0700

    exercise

--- exercise.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-username=user
-password=password
-token=$(curl -u $username:$password -X POST http://localhost:8080/token)
-curl -v -H"Authorization: Bearer $token" http://localhost:8080/greetings
-
