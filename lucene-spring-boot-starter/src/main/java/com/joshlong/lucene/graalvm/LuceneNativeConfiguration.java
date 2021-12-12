77fb28beb7e8418bce28a6a22601eb3b1ea4957d
Josh Long <josh@joshlong.com>
2021-12-12 15:58:26 -0800

    save Sun Dec 12 15:58:26 PST 2021

--- lucene-spring-boot-starter/src/main/java/com/joshlong/lucene/graalvm/LuceneNativeConfiguration.java
+++ lucene-spring-boot-starter/src/main/java/com/joshlong/lucene/graalvm/LuceneNativeConfiguration.java
@@ -1,9 +1,13 @@
 package com.joshlong.lucene.graalvm;
 
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
+@ResourceHint(patterns = { "org/commonmark/internal/util/entities.properties" })
+@NativeHint(options = "-H:+AddAllCharsets")
 @TypeHint(types = { org.apache.lucene.analysis.tokenattributes.PackedTokenAttributeImpl.class, },
 		access = { TypeAccess.DECLARED_CLASSES, TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.DECLARED_FIELDS,
 				TypeAccess.DECLARED_METHODS, })
